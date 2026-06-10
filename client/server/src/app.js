deba863f978f0189c693ffcd8b5d9e1243eb166a
OrcaAzure <klyde761@gmail.com>
2026-06-10 13:16:29 +0800

    Changes to the App.js

--- client/server/src/app.js
+++ client/server/src/app.js
@@ -1,5 +1,8 @@
-import express from "express";
-import cors from "cors";
+//Loads the Routes
+
+import express from 'express';
+import cors from 'cors';
+import authRoutes from './routes/auth.routes.js';
 
 const app = express();
 
@@ -7,8 +10,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.json({ message: "AptSpace API is running" });
+app.get('/', (req, res) => {
+  res.json({ message: 'AptSpace API is running' });
 });
+
+app.use('/api/auth', authRoutes);
 
 export default app;
---
+++ client/server/src/models/Payment.js
@@ -0,0 +1,13 @@
+// To Be fixed at a later time
+
+export default class Payment {
+  constructor(data) {
+    this.id = data.id;
+    this.booking_id = data.booking_id;
+    this.amount = data.amount;
+    this.method = data.method;
+    this.status = data.status;
+    this.created_at = data.created_at;
+    this.updated_at = data.updated_at;
+  }
+}
