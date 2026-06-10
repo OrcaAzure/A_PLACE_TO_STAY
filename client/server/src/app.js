bbb48c26f0d0b4e85898ad0f06b0e935ed878f18
OrcaAzure <klyde761@gmail.com>
2026-06-10 09:00:16 +0800

    Base Folders and Codes

---
+++ client/server/src/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({ message: "AptSpace API is running" });
+});
+
+export default app;
